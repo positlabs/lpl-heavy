7bf824fdd52f69c2388501288542f635fd78cfb9
Josh Beckwith <josh.beckwith@gmail.com>
2015-01-03 11:44:15 -0800

    removing php

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -8,3 +8,26 @@ _gaq.push(['_trackPageview']); (function() {
 	var s = document.getElementsByTagName('script')[0];
 	s.parentNode.insertBefore(ga, s);
 })();
+
+
+//facebook stuff
+
+( function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if(d.getElementById(id))
+			return;
+		js = d.createElement(s);
+		js.id = id;
+		js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=331545326882067";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
+// g+ stuff
+(function() {
+	var po = document.createElement('script');
+	po.type = 'text/javascript';
+	po.async = true;
+	po.src = 'https://apis.google.com/js/plusone.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(po, s);
+})();
